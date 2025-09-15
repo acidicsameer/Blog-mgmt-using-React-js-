0022d093d1735cfd1fa7fab43522bd2d4ed14363
Sameer Sapkota <namrmc008@gmail.com>
2025-09-15 18:08:49 +0545

    register and login authentication using mock api  successfully

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import LogSign from './authComponent/LogSign'
-
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 const Login = () => {
+ const navigate=useNavigate()
+   const handleLogin = async (data) => {
+  try {
+    const response = await axios.get("https://68c7da005d8d9f5147334a8e.mockapi.io/user", data);
+     if(response.status===200)
+     {
+        navigate('/')
+     }
+   
+  } catch (error) {
+    console.error("Error during login :", error);
+  }
+};
   return (
 <>
 
-<LogSign
+<LogSign onSubmit={handleLogin}
 name="Login"
 name2="Log in with Google "
-name3="Dont have an account?"
+name3="Don't have an account?"
 name4=" sign up"
 
 
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import LogSign from './authComponent/LogSign'
-
+import axios from 'axios';
+ import { useNavigate } from 'react-router-dom';
 const Register = () => {
+    const navigate=useNavigate()
+   const handleRegister = async (data) => {
+  try {
+    const response = await axios.post("https://68c7da005d8d9f5147334a8e.mockapi.io/user", data);
+     if(response.status===201)
+     {
+        navigate('/login')
+     }
+     else{
+         alert(" registration failed bro ")
+     }
+     
+  } catch (error) {
+    console.error("Error during register :", error);
+  }
+};
+
   return (
     <>
-<LogSign 
+   
+<LogSign onSubmit={handleRegister} 
+
 name='SignUp' 
 name2='Sign  up with Google'
  name3= " Already Sign in?"
  name4=" log in "
+
  />
-    
-    
-    
+
     </>
   )
 }
